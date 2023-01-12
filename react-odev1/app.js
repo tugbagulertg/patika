b8f0b886ce91fad973b0e5998458a37f47c0a12a
Tuğba Güler <102370994+tugbagulertg@users.noreply.github.com>
2023-01-12 16:26:31 +0300

    Add files via upload

---
+++ react-odev1/app.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const getData = async (userId) => {
+  const { data: users } = await axios(
+    "https://jsonplaceholder.typicode.com/users/" + userId
+  );
+
+  const { data: posts } = await axios(
+    `https://jsonplaceholder.typicode.com/posts?id=${userId}`
+  ); 
+
+  // user.posts 
+  users.posts= posts
+
+  try {
+    console.log(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default getData;
